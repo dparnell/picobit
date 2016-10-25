7e1e6c8d23a96f4a07fc61f6dab03b8b66730827
Cecília Carneiro e Silva <cecilia.carneiroesilva@gmail.com>
2016-10-25 14:47:49 -0200

    led-set! OK, ex2.scm

--- vm/arch/arm/cortex-m3/board-vldiscovery.c
+++ vm/arch/arm/cortex-m3/board-vldiscovery.c
@@ -7,41 +7,42 @@
 
 void halt_with_error ()
 {
-	GPIOC->ODR |= BIT(8);
+  GPIOC->ODR |= BIT(8);
 
-	while(1);
+  while(1);
 }
 
 PRIMITIVE_UNSPEC(#%sleep, arch_sleep, 1)
 {
-	static int a, b;
+  static int a, b;
 
-	a1 = decode_int (arg1);
+  a1 = decode_int (arg1);
 
-	for(a = 0; a < a1; a++) {
-		for(b = 0; b < 100; b++) {
-			__asm__ __volatile__("nop");
-		}
-	}
+  for(a = 0; a < a1; a++) {
+    for(b = 0; b < 100; b++) {
+      __asm__ __volatile__("nop");
+    }
+  }
 
-	arg1 = OBJ_FALSE;
+  arg1 = OBJ_FALSE;
 }
 
 PRIMITIVE_UNSPEC(#%set-led!, arch_set_led, 1)
 {
-	if (arg1 == OBJ_FALSE) {
-		GPIOC->ODR &= ~BIT(9);
-	} else {
-		GPIOC->ODR |= BIT(9);
-	}
+  if (arg1 == OBJ_FALSE) {
+    GPIOC->ODR &= ~BIT(8);
+  } else {
+    GPIOC->ODR |= BIT(8);
+  }
 
-	arg1 = OBJ_FALSE;
+  arg1 = OBJ_FALSE;
 }
 
 void main ()
 {
-	RCC->APB2ENR |= IOPCEN;
-	GPIOC->CRH = 0x44444411;
+  RCC->APB2ENR |= IOPCEN;
+  GPIOC->CRH   &= ~BIT(3) & ~BIT(2) & ~BIT(1);
+  GPIOC->CRH   |=  BIT(0);
 
-	interpreter();
+  interpreter();
 }
