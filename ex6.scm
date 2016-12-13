5a18bd02e09987c032cc1c683a85915a2104e47e
Cecília Carneiro e Silva <cecilia.carneiroesilva@gmail.com>
2016-12-13 09:50:44 -0200

    ex6.scm

--- ex6.scm
+++ ex6.scm
@@ -1,17 +1,18 @@
 ;;PC0 - ADC1-IN10
 ;;ADC dimerizando PB1
-;;lendo PC0 controlando PB1 - 50 Hz
+;;lendo PC0 controlando PB1 - 1k2Hz
 
 (define (config)
   (GPIO-output "C" 9)
   (ADC-config)
-  (PWM-config))
+  (PWM-config)
+  (set-psc 1))
 
 (define (main)
   (config)
   (set-led! 9 #t)
   (sleep 4000)
-  
+
   (let loop ((adc-value (ADC-read)))
     (set-duty adc-value)
     (loop (ADC-read)) )
