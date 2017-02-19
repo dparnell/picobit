a23afbb3f81854be9c510d4a727ddc4417fe5489
Cecília Carneiro e Silva <cecilia.carneiroesilva@gmail.com>
2017-02-19 10:33:31 -0300

    adc + dma, file periph/adc.c

--- vm/arch/arm/cortex-m3-HAL/include/picobit/periph/adc.c
+++ vm/arch/arm/cortex-m3-HAL/include/picobit/periph/adc.c
@@ -7,17 +7,43 @@
 #include <stm32f10x_adc.h>
 #include <stm32f10x_dma.h>
 
+#define ADC1_DR_Address    ((uint32_t)0x4001244C)
+
 PRIMITIVE(ADC_example, arch_ADC_example, 0)
 {
   ADC_InitTypeDef ADC_InitStructure;
-  
+  DMA_InitTypeDef DMA_InitStructure;
+  __IO uint16_t ADCConvertedValue;
   /* Enable peripheral clocks ------------------------------------------------*/
   /* Enable DMA1 clock */
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
 
   /* Enable ADC1 and GPIOC clock */
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOC, ENABLE);
+
+  /* DMA1 channel1 configuration ----------------------------------------------*/
+
+  DMA_DeInit(DMA1_Channel1);
+  DMA_InitStructure.DMA_PeripheralBaseAddr = ADC1_DR_Address;
+  DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)&ADCConvertedValue;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+  DMA_InitStructure.DMA_BufferSize = 1;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Disable;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+  DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+  DMA_Init(DMA1_Channel1, &DMA_InitStructure);
+  /* Enable DMA1 channel1 */
+  DMA_Cmd(DMA1_Channel1, ENABLE);
   
   ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfChannel = 1;
   ADC_Init(ADC1, &ADC_InitStructure);
 }
